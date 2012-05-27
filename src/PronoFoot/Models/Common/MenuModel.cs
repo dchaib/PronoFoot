91236f29bae2556d6a54e17c681e9faeca32fcee
Damien Chaib <dchaib@gmail.com>
2012-05-27 18:45:30 +0200

    Added dynamic menu

---
+++ src/PronoFoot/Models/Common/MenuModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PronoFoot.Models.Common
+{
+    public class MenuModel
+    {
+        public IList<CompetitionModel> Competitions { get; set; }
+
+        public MenuModel()
+        {
+            this.Competitions = new List<CompetitionModel>();
+        }
+
+        public class CompetitionModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
