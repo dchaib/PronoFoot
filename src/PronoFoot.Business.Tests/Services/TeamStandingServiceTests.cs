7e4e40e53af45d06e1aba8d7ae58627b9b27b87c
Damien Chaib <dchaib@gmail.com>
2013-10-23 22:06:56 +0200

    Added a business service to compute team standings for an edition

---
+++ src/PronoFoot.Business.Tests/Services/TeamStandingServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PronoFoot.Business.Services;
+using PronoFoot.Data;
+using PronoFoot.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PronoFoot.Business.Tests.Services
+{
+    [TestClass]
+    public class TeamStandingServiceTests
+    {
+        [TestMethod]
+        public void HomeStatisticsTest()
+        {
+            IEnumerable<Fixture> fixtures = new List<Fixture>()
+            {
+                new Fixture{ HomeTeamId=1, AwayTeamId=2, HomeTeamGoals=0, AwayTeamGoals=1},
+                new Fixture{ HomeTeamId=3, AwayTeamId=4, HomeTeamGoals=1, AwayTeamGoals=1},
+                new Fixture{ HomeTeamId=2, AwayTeamId=3, HomeTeamGoals=2, AwayTeamGoals=0},
+                new Fixture{ HomeTeamId=4, AwayTeamId=1, HomeTeamGoals=0, AwayTeamGoals=0},
+                new Fixture{ HomeTeamId=3, AwayTeamId=1, HomeTeamGoals=3, AwayTeamGoals=2},
+                new Fixture{ HomeTeamId=4, AwayTeamId=2, HomeTeamGoals=1, AwayTeamGoals=0},
+            };
+
+            var homeStats = TeamStandingService.GetHomeStatistics(fixtures);
+
+            Assert.AreEqual(4, homeStats.Count());
+
+            var homeStatsTeam3 = homeStats.SingleOrDefault(x => x.TeamId == 3);
+            Assert.IsNotNull(homeStatsTeam3);
+            Assert.AreEqual(2, homeStatsTeam3.Matches);
+            Assert.AreEqual(1, homeStatsTeam3.Wins);
+            Assert.AreEqual(1, homeStatsTeam3.Draws);
+            Assert.AreEqual(0, homeStatsTeam3.Losses);
+            Assert.AreEqual(4, homeStatsTeam3.GoalsFor);
+            Assert.AreEqual(3, homeStatsTeam3.GoalsAgainst);
+        }
+
+
+        [TestMethod]
+        public void CheckStandingsOrder()
+        {
+            IEnumerable<Fixture> fixtures = new List<Fixture>()
+            {
+                new Fixture{ HomeTeamId=1, AwayTeamId=2, HomeTeamGoals=0, AwayTeamGoals=1},
+                new Fixture{ HomeTeamId=3, AwayTeamId=4, HomeTeamGoals=1, AwayTeamGoals=1},
+                new Fixture{ HomeTeamId=2, AwayTeamId=3, HomeTeamGoals=2, AwayTeamGoals=0},
+                new Fixture{ HomeTeamId=4, AwayTeamId=1, HomeTeamGoals=0, AwayTeamGoals=0},
+                new Fixture{ HomeTeamId=3, AwayTeamId=1, HomeTeamGoals=3, AwayTeamGoals=2},
+                new Fixture{ HomeTeamId=4, AwayTeamId=2, HomeTeamGoals=1, AwayTeamGoals=0},
+            };
+
+            var fixtureRepository = new Mock<IFixtureRepository>();
+            fixtureRepository.Setup(x => x.GetFixturesForEdition(It.IsAny<int>())).Returns(() => fixtures);
+            var service = new TeamStandingService(fixtureRepository.Object);
+
+            var standings = service.GetTeamStandings(0);
+
+            Assert.AreEqual(4, standings.Count());
+
+            foreach (var item in standings)
+            {
+                switch (item.TeamId)
+                {
+                    case 1:
+                        Assert.AreEqual(4, item.Position);
+                        break;
+                    case 2:
+                        Assert.AreEqual(1, item.Position);
+                        break;
+                    case 3:
+                        Assert.AreEqual(3, item.Position);
+                        break;
+                    case 4:
+                        Assert.AreEqual(2, item.Position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/PronoFoot.Business/Services/TeamStandingService.cs
@@ -0,0 +1,96 @@
+﻿using PronoFoot.Business.Contracts;
+using PronoFoot.Business.Models;
+using PronoFoot.Data;
+using PronoFoot.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PronoFoot.Business.Services
+{
+    public class TeamStandingService : ITeamStandingService
+    {
+        private readonly IFixtureRepository fixtureRepository;
+
+        public TeamStandingService(IFixtureRepository fixtureRepository)
+        {
+            this.fixtureRepository = fixtureRepository;
+        }
+
+        public IEnumerable<TeamStanding> GetTeamStandings(int editionId)
+        {
+            var fixtures = fixtureRepository.GetFixturesForEdition(editionId);
+            var homeStats = GetHomeStatistics(fixtures);
+            var awayStats = GetAwayStatistics(fixtures);
+
+            var teamStats = from homeStat in homeStats
+                            join awayStat in awayStats on homeStat.TeamId equals awayStat.TeamId
+                            select new TeamStanding
+                            {
+                                TeamId = homeStat.TeamId,
+                                HomeStatistics = homeStat,
+                                AwayStatistics = awayStat,
+                                OverallStatistics = new TeamStatistics
+                                {
+                                    TeamId = homeStat.TeamId,
+                                    Matches = homeStat.Matches + awayStat.Matches,
+                                    Wins = homeStat.Wins + awayStat.Wins,
+                                    Draws = homeStat.Draws + awayStat.Draws,
+                                    Losses = homeStat.Losses + awayStat.Losses,
+                                    GoalsFor = homeStat.GoalsFor + awayStat.GoalsFor,
+                                    GoalsAgainst = homeStat.GoalsAgainst + awayStat.GoalsAgainst
+                                }
+                            };
+
+            int overallRank = 1;
+            var q = from teamStat in teamStats
+                    group teamStat by new { teamStat.OverallStatistics.Points, teamStat.OverallStatistics.GoalDifference, teamStat.OverallStatistics.GoalsFor } into rankGroup
+                    orderby rankGroup.Key.Points descending, rankGroup.Key.GoalDifference descending, rankGroup.Key.GoalsFor descending
+                    let currentRank = overallRank++
+                    from team in rankGroup
+                    let dummy = team.Position = currentRank
+                    select team;
+
+            return q.ToList();
+        }
+
+        public static IEnumerable<TeamStatistics> GetHomeStatistics(IEnumerable<Fixture> fixtures)
+        {
+            var q = from f in fixtures
+                    where f.HomeTeamGoals.HasValue && f.AwayTeamGoals.HasValue
+                    let gd = f.HomeTeamGoals - f.AwayTeamGoals
+                    group f by f.HomeTeamId into g
+                    select new TeamStatistics
+                    {
+                        TeamId = g.Key,
+                        Matches = g.Count(),
+                        Wins = g.Count(x => x.HomeTeamGoals > x.AwayTeamGoals),
+                        Draws = g.Count(x => x.HomeTeamGoals == x.AwayTeamGoals),
+                        Losses = g.Count(x => x.HomeTeamGoals < x.AwayTeamGoals),
+                        GoalsFor = g.Sum(x => x.HomeTeamGoals.Value),
+                        GoalsAgainst = g.Sum(x => x.AwayTeamGoals.Value)
+                    };
+            return q.AsEnumerable();
+        }
+
+        private static IEnumerable<TeamStatistics> GetAwayStatistics(IEnumerable<Fixture> fixtures)
+        {
+            var q = from f in fixtures
+                    where f.HomeTeamGoals.HasValue && f.AwayTeamGoals.HasValue
+                    group f by f.AwayTeamId into g
+                    select new TeamStatistics
+                    {
+                        TeamId = g.Key,
+                        Matches = g.Count(),
+                        Wins = g.Count(x => x.AwayTeamGoals > x.HomeTeamGoals),
+                        Draws = g.Count(x => x.AwayTeamGoals == x.HomeTeamGoals),
+                        Losses = g.Count(x => x.AwayTeamGoals < x.HomeTeamGoals),
+                        GoalsFor = g.Sum(x => x.AwayTeamGoals.Value),
+                        GoalsAgainst = g.Sum(x => x.HomeTeamGoals.Value)
+                    };
+            return q.AsEnumerable();
+        }
+    }
+}
