59daaf1bbc294e4a4e44bf5d35ae716fde6d8ca9
Damien Chaib <dchaib@gmail.com>
2015-09-21 22:57:51 +0200

    Added secondary navigation bar

--- src/PronoFoot/App_Start/BundleConfig.cs
+++ src/PronoFoot/App_Start/BundleConfig.cs
@@ -28,7 +28,8 @@ namespace PronoFoot
             //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/App/menu.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
